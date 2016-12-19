eefd33d34f901935d33a0a4fc1d48d6bfeeee4a5
Pranay Ankit <pranayankit414@gmail.com>
2016-12-19 21:44:27 +0530

    Create mapIt.py
    The programs does the following tasks:
    1. Gets a street address from the command line arguments or clipboard.
    2. Opens the web browser to the Google Maps page for the address.

---
+++ mapIt.py
@@ -0,0 +1,10 @@
+#Enter the address from command line and run the script
+
+import sys, webbrowser, pyperclip
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+    
+webbrowser.open("https://www.google.com/maps/place/" + address)
